b8870355d56cf2f8f90ba33977db80e8dbcbaf93
miguel <miguelesquirol@gmail.com>
2019-08-06 16:14:22 -0400

    Dist Folder

---
+++ dist/static/js/2.d538605465211a35b7a2.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{kV3b:function(e,s){e.exports={welcome_message:"Добре дошли във Вашият Vue.js App",name:"Miguel - FR",lastname:"Esquirol - FR",popular_links:"Популярни линкове",ecosystem:"Екосистема",pages:{about:"Страница за нас"}}}});
+//# sourceMappingURL=2.d538605465211a35b7a2.js.map
---
+++ dist/static/js/4.fdb3a834e6e972a433bf.js
@@ -0,0 +1,2 @@
+webpackJsonp([4],{en7K:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var l=function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("\n  "+e._s(e.$t("pages.about"))+"\n")])},r=[],u={render:l,staticRenderFns:r},s=u,a=t("VU/8"),c=a(null,s,!1,null,null,null);n.default=c.exports}});
+//# sourceMappingURL=4.fdb3a834e6e972a433bf.js.map
